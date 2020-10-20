bd2edb9010be9eca5f4348a9b88f3a4770860d75
Mr Cea <ogbonna.basil3@gmail.com@users.noreply.github.com>
2020-10-20 12:53:16 +0100

    add user authorization

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -6,6 +6,8 @@ import { TaskStatus } from './tasks.status.enum';
 import {Task} from './task.entity';
 import { TasksService } from './tasks.service';
 import { AuthGuard } from '@nestjs/passport';
+import { User } from 'src/auth/user.entity';
+import { GetUser } from 'src/auth/get-user.decorator';
 
 @Controller('tasks')
 @UseGuards(AuthGuard())
@@ -13,26 +15,29 @@ export class TasksController {
   constructor(private taskService: TasksService) { }
 
   @Get()
-  getTasks(@Query(ValidationPipe) filterDto:GetTaskFilterDto): Promise<Task[]> {
-    return this.taskService.getTasks(filterDto);
+  getTasks(@Query(ValidationPipe) filterDto:GetTaskFilterDto,
+  @GetUser() user:User): Promise<Task[]> {
+    return this.taskService.getTasks(filterDto,user);
   }
 
   @Get(":id")
-  getTaskById(@Param('id', ParseIntPipe) id: number): Promise<Task> {
-    return this.taskService.getTaskById(id)
+  getTaskById(@Param('id', ParseIntPipe) id: number,@GetUser() user:User): Promise<Task> {
+    return this.taskService.getTaskById(id, user)
   }
   @Delete(":id")
-  deleteTaskById(@Param('id',ParseIntPipe) id: number): void {
-    this.taskService.deleteTaskById(id)
+  deleteTaskById(@Param('id',ParseIntPipe) id: number, @GetUser() user: User): void {
+    this.taskService.deleteTaskById(id, user)
   }
   @Post()
   @UsePipes(ValidationPipe)
-  createTask(@Body() createTaskDto: CreateTaskDto): Promise<Task> {
-    return this.taskService.createTask(createTaskDto)
+  createTask(
+    @GetUser() user: User,
+    @Body() createTaskDto: CreateTaskDto): Promise<Task> {
+    return this.taskService.createTask(createTaskDto, user)
   }
   @Patch(":id/status")
-  updateTaskStatus(@Param("id",ParseIntPipe) id:number, @Body('status', TaskStatusValidationPipe) status:TaskStatus):Promise<Task>{
-    return this.taskService.updateTaskStatus(id ,status);
+  updateTaskStatus(@Param("id",ParseIntPipe) id:number, @Body('status', TaskStatusValidationPipe) status:TaskStatus, @GetUser() user: User):Promise<Task>{
+    return this.taskService.updateTaskStatus(id ,status, user);
   }
 
 }
